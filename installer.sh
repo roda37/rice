d003a00788287ec5a6f28b02284c196aa3741788
roda <roda>
2022-06-14 22:56:14 +0000

    update

--- installer.sh
+++ installer.sh
@@ -20,7 +20,7 @@ start () {
 	fi
 	echo ARCH
 	pacman -Sy vlc wget curl bpytop qemu virt-manager dnsmasq libvirt qbittorrent iwd bluez bluez-utils blueman ranger mpv git picom rofi feh xorg-xinit xorg-server xorg-xrandr xorg-xev xorg-xprop pulsemixer dosfstools terminator i3-gaps base-devel unclutter ttf-liberation ttf-dejavu bash-completion p7zip tmux awesome-terminal-fonts w3m elinks ffmpegthumbnailer highlight libcaca mediainfo odt2txt poppler ueberzug polybar i3lock steam nvidia firefox font-manager duf tldr exa dust ristretto yt-dlp dhcpcd btrfs-progs pipewire pipewire-pulse wireplumber
-	systemctl enable dhcpcd
+	systemctl enable dhcpcd libvirtd
 	echo Do you want to create a new user? [y/n]
 	read doeshe
 	if [[ "$doeshe" == "y" ]]; then
